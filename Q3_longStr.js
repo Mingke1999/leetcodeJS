b80965f4fb3d9e69f564d42f9f778a1d8cabb772
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-06-22 17:06:32 +1000

    find the longest no repeat string length using sliding window

--- Q3_longStr.js
+++ Q3_longStr.js
@@ -2,6 +2,26 @@
  * @param {string} 
  * @return {number}
  */
+//sliding window
  var lengthOfLongestSubstring = function(s) {
+    let window = "";
+    let maxStr = "";
     
+    for(let i=0;i<s.length;i++){
+        if(window.includes(s.charAt(i))==false){    //the windows does include this charater
+          window += s.charAt(i); //add to the window
+
+          
+        }else if(window.includes(s.charAt(i))==true){
+          if(window.length>maxStr.length){
+            maxStr = window;
+        }
+          window = ""; //empty the window
+          window += s.charAt(i); //add to the window
+
+        }
+    }
+
+   return maxStr.length;
 };
+
