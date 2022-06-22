5f7270f28b45417bd6533da77cd9ed6b035c9cf3
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-06-22 17:21:35 +1000

    not excepted

--- Q3_longStr.js
+++ Q3_longStr.js
@@ -3,25 +3,28 @@
  * @return {number}
  */
 //sliding window
- var lengthOfLongestSubstring = function(s) {
+ function lengthOfLongestSubstring(s) {
     let window = "";
     let maxStr = "";
-    
+   
     for(let i=0;i<s.length;i++){
         if(window.includes(s.charAt(i))==false){    //the windows does include this charater
           window += s.charAt(i); //add to the window
-
           
         }else if(window.includes(s.charAt(i))==true){
           if(window.length>maxStr.length){
             maxStr = window;
-        }
+         }
           window = ""; //empty the window
           window += s.charAt(i); //add to the window
 
         }
     }
-
-   return maxStr.length;
+    
+    let max = maxStr.length;
+    if(maxStr==""){
+        max = 1;
+    }
+   return max;
 };
 
